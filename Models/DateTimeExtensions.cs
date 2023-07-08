def7d204a474abbb2a44512c0ecd20e849c27ca7
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-07-09 01:07:41 +0200

    Adding decade and removing bin files

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -2,9 +2,15 @@
 
 public static class DateTimeExtensions
 {
+    public static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
+
     public static IEnumerable<(int year, int month)> GetYearMonths(this DateTime time) =>
         time.Year.GetYearMonths();
 
     public static IEnumerable<(int year, int month)> GetYearMonths(this int year) =>
         Enumerable.Range(1, 12).Select(month => (year, month));
+
+    public static IEnumerable<(int year, int month)> GetDecadeMonths(this DateTime time) =>
+        Enumerable.Range(time.Year.ToDecadeBeginning(), 10).SelectMany(GetYearMonths);
+
 }
