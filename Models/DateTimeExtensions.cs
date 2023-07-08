1ee14427428d500c48f4e02338d22567ff613385
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-07-09 00:58:31 +0200

    BusinessMonth

---
+++ Models/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Models;
+
+public static class DateTimeExtensions
+{
+    public static IEnumerable<(int year, int month)> GetYearMonths(this DateTime time) =>
+        time.Year.GetYearMonths();
+
+    public static IEnumerable<(int year, int month)> GetYearMonths(this int year) =>
+        Enumerable.Range(1, 12).Select(month => (year, month));
+}
