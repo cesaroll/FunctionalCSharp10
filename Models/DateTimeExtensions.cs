7dac4f3951cac5687104c81a5e84714bb20c5f16
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-07-09 17:09:26 +0200

    Designing composable types

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -2,12 +2,4 @@
 public static class DateTimeExtensions
 {
     public static Year ToYear(this DateTime dateTime) => new(dateTime.Year);
-
-    public static IEnumerable<Month> GetDecadeMonths(this DateTime dateTime) =>
-        Enumerable.Range(dateTime.Year.ToDecadeBeginning(), 10)
-            .Select(Year => new Year(Year))
-            .SelectMany(Year => Year.Months);
-
-    public static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
-
 }
