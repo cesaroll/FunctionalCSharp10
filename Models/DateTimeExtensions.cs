7d85197d4d239d75d5303a688557b53b3d9e2448
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-07-09 16:51:53 +0200

    Using object as factory of other objects

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -1,15 +1,13 @@
 ﻿namespace Models;
 public static class DateTimeExtensions
 {
-    public static IEnumerable<Month> GetYearMonths(this DateTime time) =>
-        time.Year.GetYearMonths();
+    public static Year ToYear(this DateTime dateTime) => new(dateTime.Year);
 
-    public static IEnumerable<Month> GetDecadeMonths(this DateTime time) =>
-        Enumerable.Range(time.Year.ToDecadeBeginning(), 10).SelectMany(GetYearMonths);
+    public static IEnumerable<Month> GetDecadeMonths(this DateTime dateTime) =>
+        Enumerable.Range(dateTime.Year.ToDecadeBeginning(), 10)
+            .Select(Year => new Year(Year))
+            .SelectMany(Year => Year.Months);
 
     public static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
-
-    public static IEnumerable<Month> GetYearMonths(this int year) =>
-        Enumerable.Range(1, 12).Select(month => new Month(year, month));
 
 }
---
+++ Models/Year.cs
@@ -0,0 +1,11 @@
+namespace Models;
+
+public record struct Year(int Number)
+{
+    public Month GetMonth(int ordinal) =>
+        ordinal >= 1 && ordinal <= 12  ? new(this, ordinal) : throw new ArgumentException(nameof(ordinal));
+    public IEnumerable<Month> Months =>
+        this.GetMonths(this);
+    public IEnumerable<Month> GetMonths(Year year) =>
+        Enumerable.Range(1, 12).Select(ordinal => new Month(year, ordinal));
+}
