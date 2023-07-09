c3177223c43edc067aa31dcdbf4c6cd64a2fd11f
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-07-09 22:39:48 +0200

    Adding Part and Sku

--- Models/Time/DateTimeExtensions.cs
+++ Models/Time/DateTimeExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace Models;
+﻿namespace Models.Time;
 public static class DateTimeExtensions
 {
     public static Year ToYear(this DateTime dateTime) => new(dateTime.Year);
--- Models/Time/Month.cs
+++ Models/Time/Month.cs
@@ -1,3 +1,3 @@
-namespace Models;
+namespace Models.Time;
 
 public record Month(Year Year, int Ordinal);
